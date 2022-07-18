d495a0408bb40605f426a57e51a6b870f47bca36
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-18 08:54:33 -0300

    config theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <h1>Hello</h1>
-    </div>
-  )
+    </ThemeProvider>
+  );
 }
