4badc709ad8c3baeff85f425f51820d84951dda2
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-18 12:51:56 -0300

    added styles page/History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,33 @@
+import { HistoryContainer, HistoryList, StatusTask } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>tarefa</th>
+              <th>Duração</th>
+              <th>inicio</th>
+              <th>status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Conserto de débitos técnicos </td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <StatusTask statusColor="red">Interrompido</StatusTask>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
