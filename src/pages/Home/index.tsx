5bce8b07e5b1a0e05a0fd5e597e9df26e92e29f0
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-18 11:19:41 -0300

    added styles page/Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,61 @@
+import { Play } from 'phosphor-react'
+import {
+  HomeContainer,
+  InputGroup,
+  CountdownContainer,
+  FormContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <InputGroup>
+            <label htmlFor="task">Vou trabalhar em</label>
+            <TaskInput
+              type="text"
+              id="task"
+              list="tasks-worked"
+              placeholder="nome para o seu projeto"
+            />
+
+            <datalist id="tasks-worked">
+              <option value="Calmap" />
+            </datalist>
+          </InputGroup>
+
+          <InputGroup>
+            <label htmlFor="minutesAmount">durante</label>
+            <MinutesAmountInput
+              type="number"
+              id="minutesAmount"
+              placeholder="00"
+              step={5}
+              min={5}
+              max={60}
+            />
+          </InputGroup>
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
