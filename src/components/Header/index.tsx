17ef810146c18a085db6bf3f02b35221f6bde1e2
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-07-18 10:06:05 -0300

    added styles/Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
+import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
 import { HeaderContainer } from './styles'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
+      <img src={logoIgnite} alt="ignite logo" />
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">
+              <Timer size={24} />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/history">
+              <Scroll size={24} />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,35 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    ul {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+
+      a {
+        width: 3rem;
+        height: 3rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: ${(props) => props.theme['gray-100']};
+        border-top: 3px solid transparent;
+        border-bottom: 3px solid transparent;
+        transition: border 0.1s;
+
+        &:hover {
+          border-bottom-color: ${(props) => props.theme['green-500']};
+        }
+
+        &.active {
+          color: ${(props) => props.theme['green-500']};
+        }
+      }
+    }
+  }
+`
