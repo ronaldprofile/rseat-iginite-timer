878cf3915c9de4dead71257aff7c09a6ea56005c
Ronald Tomaz <ronald@avanz.com.br>
2025-11-16 17:38:27 -0300

    style: update styled components for responsiveness and consistency

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,14 +1,21 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  width: 74rem;
-  height: calc(100vh - 10rem);
+  max-width: 74rem;
+  width: 90%;
+
   margin: 5rem auto;
   padding: 2.5rem;
   display: flex;
   flex-direction: column;
 
-  background-color: ${(props) => props.theme['gray-800']};
+  background-color: ${props => props.theme['gray-800']};
 
   border-radius: 8px;
+
+  @media (max-width: 780px) {
+    & {
+      padding: 2rem;
+    }
+  }
 `
--- src/pages/Home/components/Countdown/styles.ts
+++ src/pages/Home/components/Countdown/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
-  color: ${(props) => props.theme['gray-100']};
+  color: ${props => props.theme['gray-100']};
   line-height: 8rem;
 
   display: flex;
@@ -11,16 +11,52 @@ export const CountdownContainer = styled.div`
 
   span {
     padding: 2rem 1rem;
-    background-color: ${(props) => props.theme['gray-700']};
+    background-color: ${props => props.theme['gray-700']};
     border-radius: 8px;
+  }
+
+  @media (max-width: 780px) {
+    & {
+      font-size: 5rem;
+    }
+
+    & span {
+      padding: 1rem;
+    }
+  }
+
+  @media (max-width: 426px) {
+    & {
+      font-size: 2.5rem;
+    }
+
+    & span {
+      padding: 1rem;
+    }
   }
 `
 
 export const Separator = styled.div`
   padding: 2rem 0;
-  color: ${(props) => props.theme['green-500']};
+  color: ${props => props.theme['green-500']};
   width: 4rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
+
+  @media (max-width: 780px) {
+    & {
+      width: 2rem;
+
+      padding: 1rem;
+    }
+  }
+
+  @media (max-width: 426px) {
+    & {
+      width: 1rem;
+
+      padding: 1rem;
+    }
+  }
 `
